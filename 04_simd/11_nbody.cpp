df66783351de2b6d3bf0522580535cfaf813215a
Yuki Kobiyama <yuki.kobiyama.vn@gmail.com>
2025-05-08 01:15:41 +0900

    homework

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,26 +1,58 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <immintrin.h>
 
-int main() {
-  const int N = 8;
-  float x[N], y[N], m[N], fx[N], fy[N];
-  for(int i=0; i<N; i++) {
+int main()
+{
+  constexpr int N = 16;
+  alignas(64) float x[N], y[N], m[N], fx[N], fy[N];
+
+  for (int i = 0; i < N; ++i)
+  {
     x[i] = drand48();
     y[i] = drand48();
     m[i] = drand48();
-    fx[i] = fy[i] = 0;
+    fx[i] = fy[i] = 0.0f;
   }
-  for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
-    printf("%d %g %g\n",i,fx[i],fy[i]);
+
+  const __m512 zero_ps = _mm512_setzero_ps();
+  const __m512i idx_base = _mm512_set_epi32(
+      15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
+
+  for (int i = 0; i < N; ++i)
+  {
+    const __m512 xi = _mm512_set1_ps(x[i]);
+    const __m512 yi = _mm512_set1_ps(y[i]);
+    __m512 fx_vec = zero_ps;
+    __m512 fy_vec = zero_ps;
+
+    const __m512 xj = _mm512_load_ps(x);
+    const __m512 yj = _mm512_load_ps(y);
+    const __m512 mj = _mm512_load_ps(m);
+
+    const __m512 rx = _mm512_sub_ps(xi, xj);
+    const __m512 ry = _mm512_sub_ps(yi, yj);
+    const __m512 r2 = _mm512_fmadd_ps(ry, ry, _mm512_mul_ps(rx, rx));
+
+    __m512 inv_r = _mm512_rsqrt14_ps(r2);
+    __m512 inv_r2 = _mm512_mul_ps(inv_r, inv_r);
+    __m512 inv_r3 = _mm512_mul_ps(inv_r, inv_r2);
+
+    __m512 coef = _mm512_mul_ps(mj, inv_r3);
+
+    __m512i idx_vec = _mm512_add_epi32(idx_base, _mm512_set1_epi32(0));
+    __mmask16 self = _mm512_cmpeq_epi32_mask(idx_vec, _mm512_set1_epi32(i));
+
+    coef = _mm512_mask_blend_ps(self, coef, zero_ps);
+
+    fx_vec = _mm512_fmadd_ps(coef, rx, fx_vec);
+    fy_vec = _mm512_fmadd_ps(coef, ry, fy_vec);
+
+    fx[i] = -_mm512_reduce_add_ps(fx_vec);
+    fy[i] = -_mm512_reduce_add_ps(fy_vec);
+
+    printf("%d %g %g\n", i, fx[i], fy[i]);
   }
+  return 0;
 }
--- 06_cuda/13_bucket_sort.cpp
+++ 06_cuda/13_bucket_sort.cpp
@@ -1,32 +1,67 @@
+// bucket_sort_gpu.cu
 #include <cstdio>
 #include <cstdlib>
-#include <vector>
+#include <cuda_runtime.h>
 
-int main() {
-  int n = 50;
-  int range = 5;
-  std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
-    key[i] = rand() % range;
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+__device__ __managed__ int pos = 0;
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
+__global__ void init_bucket(int *bucket, int range)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < range)
     bucket[i] = 0;
-  }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
-  }
+}
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
+__global__ void count_keys(const int *key, int *bucket, int n)
+{
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n)
+  {
+    atomicAdd(&bucket[key[i]], 1);
+  }
+}
+
+__global__ void expand_keys(int *key, int *bucket, int range)
+{
+  int val = blockIdx.x;
+  int cnt = bucket[val];
+  while (cnt-- > 0)
+  {
+    int idx = atomicAdd(&pos, 1);
+    key[idx] = val;
+  }
+}
+
+int main()
+{
+  const int n = 50;
+  const int range = 5;
+  int *key, *bucket;
+
+  cudaMallocManaged(&key, n * sizeof(int));
+  cudaMallocManaged(&bucket, range * sizeof(int));
+
+  for (int i = 0; i < n; ++i)
+  {
+    key[i] = rand() % range;
+    printf("%d ", key[i]);
   }
   printf("\n");
+
+  int B = 256;
+  init_bucket<<<(range + B - 1) / B, B>>>(bucket, range);
+
+  count_keys<<<(n + B - 1) / B, B>>>(key, bucket, n);
+
+  expand_keys<<<range, 1>>>(key, bucket, range);
+
+  cudaDeviceSynchronize();
+
+  for (int i = 0; i < n; ++i)
+    printf("%d ", key[i]);
+  printf("\n");
+
+  cudaFree(key);
+  cudaFree(bucket);
+  return 0;
 }
